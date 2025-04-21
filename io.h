921fa79140dee83604fbdd4c103af99dca0b2bfd
yaniselidrissi <yanis.el-idrissi1@etu.univ-lorraine.fr>
2025-04-21 11:17:15 +0200

    Update io.h

--- io.h
+++ io.h
@@ -3,7 +3,11 @@
 #include "list.h"
 #include "job.h"
 
+// Affiche tous les éléments d'une liste en utilisant une fonction de callback
 void view_list(list_t* L, void (*ptrf)(void*));
+
+// Trie la liste avec un tri rapide (quicksort) et une fonction de comparaison
 void quick_sort(list_t* L, int (*cmpFct)(void*, void*));
-int iDegreeJobCmp(void* A, void* B);
+
+// Lecture d’un graphe de dépendances depuis un fichier .dta
 list_t* read_graph(char* path);
