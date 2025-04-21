2870f51e16110785dbb85d6cceb865fea23d84d0
yaniselidrissi <yanis.el-idrissi1@etu.univ-lorraine.fr>
2025-04-21 11:19:28 +0200

    Update job_2.h

--- job_2.h
+++ job_2.h
@@ -1,19 +1,10 @@
 #pragma once
+#include "job_1.h"
 
-job_t * new_empty_job();
-job_t * new_job(char * title);
-void free_job(job_t ** ptrJ);
-
-void view_job(void* ptrJ);
-
-
-char * get_job_title(job_t * J);
-void set_job_title(job_t * J, char * title);
-
-double get_job_life(job_t * J);
-void set_job_life(job_t * J, double life);
-
-int get_job_iDegree(job_t * J);
-void set_job_iDegree(job_t * J, int iDegree);
-void incr_job_iDegree(job_t * J);
-void decr_job_iDegree(job_t * J);
+// Spécifiques à job
+int get_job_iDegree(job_t* J);
+int get_job_oDegree(job_t* J);
+void incr_job_iDegree(job_t* J);
+void incr_job_oDegree(job_t* J);
+void decr_job_iDegree(job_t* J);
+void decr_job_oDegree(job_t* J);
