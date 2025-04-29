77eee38d4708bb8de7f8be0e23de1a2525d5ac80
yaniselidrissi <yanis.el-idrissi1@etu.univ-lorraine.fr>
2025-04-29 14:22:18 +0200

    Update rank.c

--- rank.c
+++ rank.c
@@ -1,16 +1,15 @@
-
 #include "job.h"
 #include "list.h"
-#include <string.h>
 #include <stdlib.h>
-#include <stdbool.h>
 #include <float.h>
-#define UNDEF -9999
+#include <stdbool.h>
 
-
+// Calcul des rangs selon algorithme topologique
 void ranking(list_t* G) {
+    // File des prêts à traiter
     list_t* ready = new_list();
 
+    // Initialisation des degrés entrants dynamiques
     for (list_elm_t* E = get_head(G); E; E = get_suc(E)) {
         job_t* J = get_data(E);
         J->dyn_input_degree = J->input_degree;
@@ -19,19 +18,21 @@ void ranking(list_t* G) {
         }
     }
 
+    // Parcours en ordre topo
     while (!is_empty(ready)) {
-        job_t* J = (job_t*)take_out_front(ready);
-
+        job_t* J = take_out(ready);
+        // Calcul du rang : max des prédécesseurs +1
         int max_rank = -1;
-        for (list_elm_t* E = get_head(J->precedence); E; E = get_suc(E)) {
-            job_t* pred = get_data(E);
-            if (pred->rank > max_rank)
+        for (list_elm_t* P = get_head(J->precedence); P; P = get_suc(P)) {
+            job_t* pred = get_data(P);
+            if (pred->rank > max_rank) {
                 max_rank = pred->rank;
+            }
         }
         J->rank = max_rank + 1;
-
-        for (list_elm_t* E = get_head(J->posteriority); E; E = get_suc(E)) {
-            job_t* succ = get_data(E);
+        // Mise à jour des successeurs
+        for (list_elm_t* S = get_head(J->posteriority); S; S = get_suc(S)) {
+            job_t* succ = get_data(S);
             succ->dyn_input_degree--;
             if (succ->dyn_input_degree == 0) {
                 queue(ready, succ);
@@ -39,137 +40,105 @@ void ranking(list_t* G) {
         }
     }
 
+    // Libération de la file
     del_list(&ready, NULL);
 }
 
+// Test récursif pour élagage
 static bool reachable(job_t* src, job_t* target) {
     if (src == target) return true;
-
     for (list_elm_t* E = get_head(src->posteriority); E; E = get_suc(E)) {
-        job_t* succ = get_data(E);
-        if (reachable(succ, target)) return true;
+        if (reachable(get_data(E), target)) return true;
     }
-
     return false;
 }
 
+// Suppression des arcs redondants
 void prune(list_t* G) {
     for (list_elm_t* E = get_head(G); E; E = get_suc(E)) {
         job_t* J = get_data(E);
-        list_elm_t* next = NULL;
-
-        for (list_elm_t* S = get_head(J->posteriority); S; S = next) {
-            next = get_suc(S);
-            job_t* succ = get_data(S);
-
-            remove_item(J->posteriority, succ);
+        list_elm_t* cur = get_head(J->posteriority);
+        while (cur) {
+            list_elm_t* next = get_suc(cur);
+            job_t* succ = get_data(cur);
+            // Temporarily remove
+            take_out(J->posteriority);
             remove_item(succ->precedence, J);
-            decr_job_oDegree(J);
-            decr_job_iDegree(succ);
-
+            J->output_degree--;
+            succ->input_degree--;
+            // Reinsert if still necessary
             if (!reachable(J, succ)) {
                 ordered_insert(J->posteriority, succ, &titleJobCmp);
                 ordered_insert(succ->precedence, J, &titleJobCmp);
-                incr_job_oDegree(J);
-                incr_job_iDegree(succ);
+                J->output_degree++;
+                succ->input_degree++;
             }
+            cur = next;
         }
     }
 }
 
+// Calcul des dates et marges
 void marges(list_t* G) {
+    // Initialisation au plus tôt
     for (list_elm_t* E = get_head(G); E; E = get_suc(E)) {
         job_t* J = get_data(E);
-        J->au_plus_tot = UNDEF;
-        J->au_plus_tard = UNDEF;
+        J->au_plus_tot = 0;
     }
-
-    bool changed;
-    do {
-        changed = false;
-        for (list_elm_t* E = get_head(G); E; E = get_suc(E)) {
-            job_t* J = get_data(E);
-            if (get_job_iDegree(J) == 0) {
-                if (J->au_plus_tot == UNDEF) {
-                    J->au_plus_tot = 0;
-                    changed = true;
-                }
-            }
-            else {
-                double max_pred = -1;
-                bool all_pred_ready = true;
-                for (list_elm_t* P = get_head(J->precedence); P; P = get_suc(P)) {
-                    job_t* pred = get_data(P);
-                    if (pred->au_plus_tot == UNDEF) {
-                        all_pred_ready = false;
-                        break;
-                    }
-                    double fin_pred = pred->au_plus_tot + pred->life;
-                    if (fin_pred > max_pred) max_pred = fin_pred;
-                }
-                if (all_pred_ready && J->au_plus_tot != max_pred) {
-                    J->au_plus_tot = max_pred;
-                    changed = true;
-                }
-            }
-        }
-    } while (changed);
-
-    double max_end = 0;
+    // Propagation au plus tôt
     for (list_elm_t* E = get_head(G); E; E = get_suc(E)) {
         job_t* J = get_data(E);
-        double fin = J->au_plus_tot + J->life;
-        if (fin > max_end) max_end = fin;
+        for (list_elm_t* S = get_head(J->posteriority); S; S = get_suc(S)) {
+            job_t* succ = get_data(S);
+            double candidate = J->au_plus_tot + J->life;
+            if (candidate > succ->au_plus_tot) {
+                succ->au_plus_tot = candidate;
+            }
+        }
     }
+    // Date de fin du projet
+    double project_end = 0;
     for (list_elm_t* E = get_head(G); E; E = get_suc(E)) {
         job_t* J = get_data(E);
-        if (get_job_oDegree(J) == 0) {
-            J->au_plus_tard = max_end - J->life;
+        double end = J->au_plus_tot + J->life;
+        if (end > project_end) project_end = end;
+    }
+    // Initialisation au plus tard
+    for (list_elm_t* E = get_head(G); E; E = get_suc(E)) {
+        job_t* J = get_data(E);
+        if (J->posteriority->numelm == 0) {
+            J->au_plus_tard = project_end - J->life;
+        }
+        else {
+            J->au_plus_tard = project_end;
         }
     }
-
-    do {
-        changed = false;
-        for (list_elm_t* E = get_tail(G); E; E = get_pred(E)) {
-            job_t* J = get_data(E);
-            if (get_job_oDegree(J) == 0) continue;
-
-            bool all_succ_ready = true;
-            double min_succ = DBL_MAX;
-            for (list_elm_t* S = get_head(J->posteriority); S; S = get_suc(S)) {
-                job_t* succ = get_data(S);
-                if (succ->au_plus_tard == UNDEF) {
-                    all_succ_ready = false;
-                    break;
-                }
-                double tard = succ->au_plus_tard - J->life;
-                if (tard < min_succ) min_succ = tard;
-            }
-            if (all_succ_ready && J->au_plus_tard != min_succ) {
-                J->au_plus_tard = min_succ;
-                changed = true;
+    // Propagation au plus tard
+    for (list_elm_t* E = get_tail(G); E; E = get_pred(E)) {
+        job_t* J = get_data(E);
+        for (list_elm_t* P = get_head(J->posteriority); P; P = get_suc(P)) {
+            job_t* succ = get_data(P);
+            double candidate = succ->au_plus_tard - J->life;
+            if (candidate < J->au_plus_tard) {
+                J->au_plus_tard = candidate;
             }
         }
-    } while (changed);
-
+    }
+    // Calcul des marges
     for (list_elm_t* E = get_head(G); E; E = get_suc(E)) {
         job_t* J = get_data(E);
         J->marge_totale = J->au_plus_tard - J->au_plus_tot;
-
-        double min_succ_start = DBL_MAX;
-        for (list_elm_t* S = get_head(J->posteriority); S; S = get_suc(S)) {
-            job_t* succ = get_data(S);
-            double val = succ->au_plus_tot;
-            if (val < min_succ_start) min_succ_start = val;
-        }
-
-        if (get_job_oDegree(J) == 0) {
+        if (J->posteriority->numelm == 0) {
             J->marge_libre = J->marge_totale;
         }
         else {
-            J->marge_libre = min_succ_start - J->au_plus_tot - J->life;
+            double min_start = DBL_MAX;
+            for (list_elm_t* S = get_head(J->posteriority); S; S = get_suc(S)) {
+                job_t* succ = get_data(S);
+                if (succ->au_plus_tot < min_start) min_start = succ->au_plus_tot;
+            }
+            J->marge_libre = min_start - (J->au_plus_tot + J->life);
         }
-
         J->critique = (J->marge_totale == 0);
     }
 }
