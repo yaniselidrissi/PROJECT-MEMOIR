72413ef900d3e9ca0c753f820432a7157c8fe9b4
yaniselidrissi <yanis.el-idrissi1@etu.univ-lorraine.fr>
2025-04-30 08:23:25 +0200

    Update io.h

--- io.h
+++ io.h
@@ -1,13 +1,13 @@
-#pragma once
+	#pragma once
 
-#include "list.h"
-#include "job.h"
+	#include "list.h"
+	#include "job.h"
 
-// Affiche tous les éléments d'une liste en utilisant une fonction de callback
-void view_list(list_t* L, void (*ptrf)(void*));
+	// Affiche tous les éléments d'une liste en utilisant une fonction de callback
+	void view_list(list_t* L, void (*ptrf)(void*));
 
-// Trie la liste avec un tri rapide (quicksort) et une fonction de comparaison
-void quick_sort(list_t* L, int (*cmpFct)(void*, void*));
+	// Trie la liste avec un tri rapide (quicksort) et une fonction de comparaison
+	void quick_sort(list_t* L, int (*cmpFct)(void*, void*));
 
-// Lecture d’un graphe de dépendances depuis un fichier .dta
-list_t* read_graph(char* path);
+	// Lecture d’un graphe de dépendances depuis un fichier .dta
+	list_t* read_graph(char* path);
